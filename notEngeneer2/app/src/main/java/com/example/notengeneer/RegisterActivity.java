79620b1ce6bb08dbc31c6575b71ff1c2d58595b3
filipposantoro26 <filipposantoro26@gmail.com>
2022-11-24 12:10:18 +0100

    login/registrazione finiti

--- notEngeneer2/app/src/main/java/com/example/notengeneer/RegisterActivity.java
+++ notEngeneer2/app/src/main/java/com/example/notengeneer/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,6 +33,13 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText etCap;
     TextView tvLoginHere;
     Button btnRegister;
+    TextInputEditText etName;
+    TextInputEditText etSurname;
+    TextInputEditText etState;
+    TextInputEditText etAddress;
+    TextInputEditText etAddressNumber;
+    DatePicker dateofBird;
+
 
     FirebaseAuth mAuth;
     FirebaseFirestore firestore;
@@ -41,11 +50,20 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        etRegEmail = findViewById(R.id.etRegEmail);
-        etRegPassword = findViewById(R.id.etRegPass);
+        etRegEmail = findViewById(R.id.etRegEmail); //required
+        etRegPassword = findViewById(R.id.etRegPass);//required
         tvLoginHere = findViewById(R.id.tvLoginHere);
         btnRegister = findViewById(R.id.btnRegister);
-        etCap = findViewById(R.id.etCap);
+        etCap = findViewById(R.id.etCap);//required
+
+        etName =findViewById(R.id.Name);
+        etSurname = findViewById(R.id.Surname);
+        etState =findViewById(R.id.State);//required
+        etAddress = findViewById(R.id.Address);//required
+        etAddressNumber = findViewById(R.id.nAddress);//required
+        dateofBird=findViewById(R.id.DateBirth);
+
+
 
         mAuth = FirebaseAuth.getInstance();
         firestore =FirebaseFirestore.getInstance();
@@ -62,6 +80,16 @@ public class RegisterActivity extends AppCompatActivity {
         String email = etRegEmail.getText().toString();
         String password = etRegPassword.getText().toString();
         String Cap = etCap.getText().toString();
+        String name = etName.getText().toString();
+        String surname = etSurname.getText().toString();
+        String state = etState.getText().toString();
+        String address = etAddress.getText().toString();
+        String addressNumber = etAddressNumber.getText().toString();
+        String dateBirth = dateofBird.getDayOfMonth()+"/"+((dateofBird.getMonth())<10?"0":"")+(dateofBird.getMonth()+1)+"/"+dateofBird.getYear();
+
+
+
+
 
         if (TextUtils.isEmpty(email)){
             etRegEmail.setError("Email cannot be empty");
@@ -72,6 +100,15 @@ public class RegisterActivity extends AppCompatActivity {
         }else if(TextUtils.isEmpty(Cap)) {
             etRegPassword.setError("Cap cannot be empty");
             etRegPassword.requestFocus();
+        }else if(TextUtils.isEmpty(address)) {
+            etAddress.setError("Cap cannot be empty");
+            etAddress.requestFocus();
+        }else if(TextUtils.isEmpty(addressNumber)) {
+            etAddressNumber.setError("Cap cannot be empty");
+            etAddressNumber.requestFocus();
+        }else if(TextUtils.isEmpty(state)) {
+            etState.setError("Cap cannot be empty");
+            etState.requestFocus();
         }else{
                 mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
@@ -80,7 +117,15 @@ public class RegisterActivity extends AppCompatActivity {
                             //crea tabella user con cap
                             Map<String,Object> users = new HashMap<>();
                             users.put("email",email);
+                            users.put("name",name);
+                            users.put("surname",surname);
+                            users.put("state",state);
                             users.put("CAP",Cap);
+                            users.put("address",address);
+                            users.put("address_number",addressNumber);
+                            users.put("date_birth",dateBirth);
+                            //users.put("CAP",Cap); //data
+
                             firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                                 @Override
                                 public void onSuccess(DocumentReference documentReference) {
